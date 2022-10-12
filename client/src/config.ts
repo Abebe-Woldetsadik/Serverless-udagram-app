32b0556a185d422bc379e1d696deb4a4d1c0eb20
Abebe <69292554+Abebe-Woldetsadik@users.noreply.github.com>
2022-10-12 07:54:05 +0300

    Adding apiId and Auth0 to config.ts in client folder

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,11 @@ const apiId = '247ohwy456';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-qog-qnpw.us.auth0.com',
-  clientId: 'hC06zNYwlwJyHTUGLx1EUxbDCtrBji31',
+  domain: 'dev-qog-qnpw.us.auth0.com',   // Auth0 domain
+  clientId: 'hC06zNYwlwJyHTUGLx1EUxbDCtrBji31', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 };
+
+
+//apiId = 247ohwy456
+//jwksUrl = 'https://dev-qog-qnpw.us.auth0.com/.well-known/jwks.json'
