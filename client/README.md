3f41de686b001aa7db7351426bd99e7afcc4fdc0
Abebe <69292554+Abebe-Woldetsadik@users.noreply.github.com>
2022-10-12 07:42:41 +0300

    message

---
+++ client/README.md
@@ -0,0 +1,3 @@
+
+
+front end application code
---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '247ohwy456';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-qog-qnpw.us.auth0.com',
+  clientId: 'hC06zNYwlwJyHTUGLx1EUxbDCtrBji31',
+  callbackUrl: 'http://localhost:3000/callback'
+};
